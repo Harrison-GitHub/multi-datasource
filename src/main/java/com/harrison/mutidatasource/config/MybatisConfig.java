5d6da1cdfd1987fc97c1c3463659f6a935e903c4
hanyu <hanyu@neucloud.cn>
2020-12-30 10:41:06 +0800

    delete useless configuration

--- src/main/java/com/harrison/mutidatasource/config/MybatisConfig.java
+++ src/main/java/com/harrison/mutidatasource/config/MybatisConfig.java
@@ -1,6 +1,5 @@
 package com.harrison.mutidatasource.config;
 
-import com.alibaba.druid.pool.DruidDataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
